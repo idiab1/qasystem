923798b261cc366669ff2efe33b8f2c93de2ad1c
islamdiab-stack <islamdiab967@gmail.com>
2021-04-18 05:13:28 +0200

    Create new migration for posts and add column like user_id for relationship between posts and users, title, description, and softdeletes and added all column for comments like posts just add new column for parent_id this column for relationship between comment to replies comments

---
+++ database/migrations/2021_04_17_153439_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('title');
+            $table->longText('description');
+            $table->string('image')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
