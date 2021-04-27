940e870a964addf1e48e5af91e85caf68d72a5e2
islamdiab-stack <islamdiab967@gmail.com>
2021-04-27 06:02:58 +0200

    Add Container for breadcrumb

--- resources/views/layouts/breadcrumb.blade.php
+++ resources/views/layouts/breadcrumb.blade.php
@@ -1,5 +1,13 @@
-<div aria-label="breadcrumb">
-    <ol class="breadcrumb">
-        @yield('breadcrumb-items')
-    </ol>
+<div class="breadcrumb">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        @yield('breadcrumb-items')
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
