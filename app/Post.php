31b4e972901df03d1948fe20a3aeb286a026b15d
islamdiab-stack <islamdiab967@gmail.com>
2021-04-28 06:55:46 +0200

    Add slug column in fillable array and add relationship method for belongs to User model

--- app/Post.php
+++ app/Post.php
@@ -10,8 +10,19 @@ class Post extends Model
     use softDeletes;
     protected $dates = ['deleted_at'];
     protected $fillable = [
-        'user_id', 'title', 'description', 'image'
+        'user_id', 'title', 'description', 'image', 'slug'
     ];
+
+    /**
+     * Get the user that owns the Post
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'user_id');
+    }
+
     /**
      * Get all of the comments for the Post
      *
