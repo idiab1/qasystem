0c9a3e89f1a937b4bb7aed66a899376f96b7bcad
islamdiab-stack <islamdiab967@gmail.com>
2021-05-03 01:04:56 +0200

    Create store method for store data to database table and validate data while coming from user and add to comment table in database

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Comment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CommentController extends Controller
 {
@@ -35,7 +36,18 @@ class CommentController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // dd($request);
+        $request->validate([
+            'description' => 'required',
+        ]);
+
+        $comment = new Comment();
+        $comment->description = $request->description;
+        $comment->user_id = Auth::user()->id;
+        $comment->post_id = $request->post_id;
+        $comment->save();
+
+        return redirect()->back();
     }
 
     /**
