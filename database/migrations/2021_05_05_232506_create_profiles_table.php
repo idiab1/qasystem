7b666111bcd767c26510c95b8e33b71c28d1e442
islamdiab-stack <islamdiab967@gmail.com>
2021-05-06 01:39:04 +0200

    Create new migration for profile where i created 7 columns for id, user_id, gender, address, facebook_url, and linkedin_url

---
+++ database/migrations/2021_05_05_232506_create_profiles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('gender')->nullable();
+            $table->string('address')->nullable();
+            $table->text('bio')->nullable();
+            $table->text('facebook_url')->nullable();
+            $table->text('linkedin_url')->nullable();
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
