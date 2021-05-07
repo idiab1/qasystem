854daf9fc88df63c461710de30745d210d4757d0
islamdiab-stack <islamdiab967@gmail.com>
2021-05-07 02:41:48 +0200

    Edit on name of route profile setting

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -46,7 +46,7 @@
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                             <!-- Logout route -->
-                            <a class="dropdown-item" href="{{ route('profile.index') }}">
+                            <a class="dropdown-item" href="{{ route('profile.setting') }}">
                                 <i class="fas fa-cogs"></i> {{ __('Profile Setting') }}
                             </a>
                             <!-- Logout route -->
